66ec5b160072d5c2ce62db142c4ac67dbf43c98a
Joao Marcelo Silva <joaom5635@gmail.com>
2021-10-12 11:58:22 -0300

    alteracao
    nome e senha do wifi

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #include <FS_File_Record.h> // Nossa lib personalizada do SPIFFS
 #include <esp_task_wdt.h>
 
-#define SSID "nome_da_rede"
-#define PASSWORD "senha_da_rede"
+#define SSID "trem"
+#define PASSWORD "ze127naide"
 #define led 17
 #define botao 16
 #define host_google "www.google.com"
