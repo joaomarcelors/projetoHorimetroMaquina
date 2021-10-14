367d0d68bb5572d6207cf320fba6db9b94da90d3
Joao Marcelo Silva <joaom5635@gmail.com>
2021-10-14 12:08:00 -0300

    alteracao de variavel

--- tresBotoes/src/main.cpp
+++ tresBotoes/src/main.cpp
@@ -8,14 +8,14 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
-  pinMode(19, OUTPUT);
+  pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led1, HIGH);
-  digitalWrite(19, HIGH);
+  digitalWrite(led2, HIGH);
   digitalWrite(led3, HIGH);
   delay(500);
   digitalWrite(led1, LOW);
